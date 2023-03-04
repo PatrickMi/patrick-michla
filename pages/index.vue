5e46aefb34872ff951387a72e3c10888983c8687
Patrick Michla <pmichla@gmail.com>
2023-03-04 14:58:54 +0100

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <img src="images/portrait.jpg"/>
+    <h1>Patrick Michla</h1>
+    <h2>Software Engineer | Studied at University of Kassel | Founding maevsi</h2>
+    <p>Kassel, Germany</p>
+
+    <h3>Current Work</h3>
+    <ul>
+        <li>Founding maevsi, the innovative event-app that is coming soon</li>
+    </ul>
+
+    <h3>Education</h3>
+    <ul>
+        <li>University of Kassel: Bachelor of Science</li>
+    </ul>
+
+    <h3>Work experience</h3>
+    <ul>
+        <li>Holder of the "Hessen-Ideen" scholarship that assists young founders</li>
+        <li>Yatta Solutions: Working Student in Software Engineering</li>
+        <li>University of Kassel: Research Assistant</li>
+    </ul>
+
+    <h3>Languages</h3>
+    <ul>
+        <li>German: native proficiency</li>
+        <li>English: professional working proficiency</li>
+        <li>Polish: limited working proficiency</li>
+    </ul>
+
+    <h3>Social Media</h3>
+    <ul>
+        <li><a href="http://www.maev.si">maevsi</a></li>
+        <li><a href="http://www.linkedin.com/in/patrick-michla">LinkedIn</a></li>
+        <li><a href="http://www.instagram.com/patricks_01">Instagram</a></li>
+    </ul>
+</template>
+
+<style>
+img {
+    height: 300px;
+}
+</style>
